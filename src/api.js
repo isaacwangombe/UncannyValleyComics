f4f519834dcb11b7f808dc4a9cde988a69e0591a
isaac <isaac.wangombe@ukiyosoftware.com>
2025-10-30 18:01:46 +0300

    Edits on apis base

--- src/api.js
+++ src/api.js
@@ -1,5 +1,10 @@
 // src/api.js
-const API_BASE = import.meta.env.VITE_API_URL;
+export const BACKEND_BASE =
+  import.meta.env.MODE === "production"
+    ? "https://uncanny-valley-comics-backend.onrender.com"
+    : "http://127.0.0.1:8000";
+
+export const API_BASE = `${BACKEND_BASE}/api`;
 
 /* ------------------------------------------
    🧩 Helpers for CSRF + authenticated requests
